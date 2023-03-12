77e51c184766f2a5de7db39c5a3db17c5bd10712
Naveen-369 <nk9363690@gmail.com>
2023-03-12 15:49:31 +0530

    Finished push pop and top functions

--- stacks.c
+++ stacks.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-int stack[7];
+/*Declaration of Push, pop and top function for stack operation*/
+const int MAX=12;
+int stack[12];
 int top=-1;
-void push(int data){
-    if (top==4){
-        printf("Stack overflow\n");
-        return;
-    }
-    else{
-        top+=1;
-        stack[top]=data;
-    }
-}
-void pop(){
-    if(top==-1){
-        printf("Stack underflow\n");
-        }
-    else{printf("\n%d",stack[top]);
-    top-=1;
-    return;}
-}
+void push(int);
+void pop();
+void top_e();
 int main(){
     push(5);
     push(8);
@@ -27,4 +14,31 @@ int main(){
     push(2);
     pop();
     pop();
+    top_e();
+    }
+
+void push(int data){
+    if (top==MAX){
+        printf("\nStack overflow ! ! ! ! ! ! ! ! !\n");
+        return;
+    }
+    else{
+        top+=1;
+        stack[top]=data;
+    }
+}
+
+void pop(){
+    if(top==-1){
+        printf("\nStack underflow ! ! ! ! ! ! ! ! ! ! ! ! !\n");
+        }
+    else{
+        printf("\n%d",stack[top]);
+        top-=1;
+        return;}
+}
+
+void top_e(){
+    printf("\n%d at the position %d",stack[top],top);
+    return;
 }
